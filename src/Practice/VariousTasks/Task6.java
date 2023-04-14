60a174b73a58ed69493b3a0e8ebf9fa906df67c1
rozemaryn <animalitos@yandex.ru>
2023-04-14 16:05:10 +0300

    update Task6

--- src/Practice/VariousTasks/Task6.java
+++ src/Practice/VariousTasks/Task6.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/*
+Create a method that takes a string list as a parameter,
+then returns the list sorted alphabetically from Z to A.
+ */
+
 public class Task6 {
     public static ArrayList <String> sortList(List <String> list) {
-        Collections.sort(list);
+        list.sort(Collections.reverseOrder());
         return (ArrayList<String>) list;
     }
 
